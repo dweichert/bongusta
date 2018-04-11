5569ad56be0ac2c2d0ce30b5afbc52fe757865db
David Weichert <info@davidweichert.de>
2018-04-11 20:27:39 +0200

    Adds created and updated to Ingredient entity.

--- src/Entity/Ingredient.php
+++ src/Entity/Ingredient.php
@@ -2,9 +2,11 @@
 
 namespace App\Entity;
 
+use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\IngredientRepository")
@@ -37,6 +39,18 @@ class Ingredient
      * @ORM\ManyToMany(targetEntity="App\Entity\Recipe", mappedBy="ingredients")
      */
     private $recipes;
+
+    /**
+     * @Gedmo\Timestampable(on="update")
+     * @ORM\Column(type="datetime")
+     */
+    private $updated;
+
+    /**
+     * @Gedmo\Timestampable(on="create")
+     * @ORM\Column(type="datetime")
+     */
+    private $created;
 
     public function __construct()
     {
@@ -80,6 +94,31 @@ class Ingredient
     public function setLink(?string $link): self
     {
         $this->link = $link;
+
+        return $this;
+    }
+
+
+    public function getCreated(): DateTime
+    {
+        return $this->created;
+    }
+
+    public function setCreated(DateTime $created): self
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    public function getUpdated(): DateTime
+    {
+        return $this->updated;
+    }
+
+    public function setUpdated(DateTime $updated): self
+    {
+        $this->updated = $updated;
 
         return $this;
     }
